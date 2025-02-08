620f99a176633c7f4f6af51d32b0a06ba8884c49
hendisantika <hendisantika@yahoo.co.id>
2025-02-09 06:49:45 +0700

    fix: Add LibraryUtility🫸🌀✏️📗🐧🐳⬆

---
+++ library/src/main/kotlin/LibraryUtility.kt
@@ -0,0 +1,20 @@
+package id.my.hendisantika
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-multi-module-gradle-example
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/02/25
+ * Time: 06.48
+ * To change this template use File | Settings | File Templates.
+ */
+class LibraryUtility {
+    companion object {
+        fun getFoo(): String {
+            return "bar"
+        }
+    }
+}

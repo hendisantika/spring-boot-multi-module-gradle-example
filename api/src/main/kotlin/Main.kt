036b7d431a78acc88c361872d8ae99ba40468f7c
hendisantika <hendisantika@yahoo.co.id>
2025-02-09 06:53:35 +0700

    feat: Add api module🫸🌀✏️📗🐧🐳⬆

---
+++ api/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+package com.example
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Default (Template) Project
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 09/02/25
+ * Time: 06.52
+ * To change this template use File | Settings | File Templates.
+ *///TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    println("Hello, " + name + "!")
+
+    for (i in 1..5) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        println("i = $i")
+    }
+}

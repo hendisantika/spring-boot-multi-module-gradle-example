1e0b000802019cd01fca7f1b804db224b5042d65
hendisantika <hendisantika@yahoo.co.id>
2025-02-09 06:30:22 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/kotlin/id/my/hendisantika/springbootmultimodulegradleexample/SpringBootMultiModuleGradleExampleApplication.kt
@@ -0,0 +1,11 @@
+package id.my.hendisantika.springbootmultimodulegradleexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootMultiModuleGradleExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootMultiModuleGradleExampleApplication>(*args)
+}
---
+++ src/test/kotlin/id/my/hendisantika/springbootmultimodulegradleexample/SpringBootMultiModuleGradleExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootmultimodulegradleexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootMultiModuleGradleExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
